05c75b8759949d1fa8f36fd6220664d13374c699
Serubin <Serubin@Serubin.net>
2018-02-03 10:51:28 -0500

    Update dependencies & Add test suite

---
+++ test/unit/setup.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import ajax from 'vue-h-ajax';
+
+// Setup mock localStorage
+global.window = {}
+import localStorage from 'mock-local-storage'
+window.localStorage = global.localStorage
+
+// Init vuex store
+import store from '@/store'
+global.store = store; // Globalize store
+
+// Setup ajax
+Vue.use(ajax)
+Vue.http.withCredentials = false;
+
+// Define dummy mdl directive
+Vue.directive('mdl', {
+    bind (el) {
+        return;
+    }
+})
+
+Vue.config.productionTip = false
